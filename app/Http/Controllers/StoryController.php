87bdbf8d9384941201a279f80800098427278d85
Sykamiotis Aggelos <a.sykamiotis@gmail.com>
2023-11-30 13:34:52 +0200

    toImage function

--- app/Http/Controllers/StoryController.php
+++ app/Http/Controllers/StoryController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class StoryController extends Controller
 {
-    //
     public function toImage(Request $request){
-        dd($request->get('toImage'));
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ',
+        ])->post('https://api.openai.com/v1/audio/translations',
+                [
+                        "model"=> "gpt-3.5-turbo",
+                        "input"=> "The quick brown fox jumped over the lazy dog.",
+                        "voice"=> "alloy"
+                ]);
+
+        dd($response);
+
     }
     public function toSpeech(Request $request){
         dd($request->get('toSpeech'));
